174fb75a197016c4a143c73d67454b2a7cfe6d13
Nii Ashikwei Tetteh <niiashikwei@hotmail.com>
2019-05-31 22:15:44 -0400

    list view

--- App.js
+++ App.js
@@ -1,54 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-//import PresentationalComponent from "./PresentationalComponent";
-//
-// export default class App extends React.Component {
-//   state = {
-//     myState: 'This is my state'
-//   }
-//
-//   render() {
-//     return (
-//       <View>
-//         <PresentationalComponent myState = {this.state.myState} />
-//       </View>
-//     );
-//   }
-// }
+import List from './List.js'
 
-const Home = (props) => {
+const App = () => {
   return (
-      <View style = {styles.container}>
-        <View style = {styles.redbox} />
-        <View style = {styles.bluebox} />
-        <View style = {styles.blackbox} />
-      </View>
+      <List />
   )
 }
-
-export default Home
-
-const styles = StyleSheet.create ({
-  container: {
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    alignItems: 'flex-end',
-    backgroundColor: 'grey',
-    height: 600
-  },
-  redbox: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'red'
-  },
-  bluebox: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'blue'
-  },
-  blackbox: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'black'
-  },
-})
+export default App
