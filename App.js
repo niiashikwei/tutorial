c9a180647106a2a6531b9938c1f231cc0ec9771c
Nii Ashikwei Tetteh <niiashikwei@hotmail.com>
2019-06-08 14:26:17 -0400

    switch

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
-import React, { Component } from 'react';
-import { StatusBar } from 'react-native'
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import SwitchExample from './switch_example.js'
 
-const App = () => {
-    return (
-        <StatusBar barStyle = "dark-content" hidden = {false} backgroundColor = "#00BCD4" translucent = {true}> foobar</StatusBar>
-    )
+export default class HomeContainer extends Component {
+    constructor() {
+        super();
+        this.state = {
+            switch1Value: false,
+        }
+    }
+    toggleSwitch1 = (value) => {
+        this.setState({switch1Value: value})
+        console.log('Switch 1 is: ' + value)
+    }
+    render() {
+        return (
+            <View>
+                <SwitchExample
+                    toggleSwitch1 = {this.toggleSwitch1}
+                    switch1Value = {this.state.switch1Value}/>
+            </View>
+        );
+    }
 }
-export default App
